7dcae8599796bac3ad7acd9a33f6325b62eb5778
Vlad Kraevskiy <vladkraevskii@gmail.com>
2016-10-14 00:19:29 +0300

    added new view

---
+++ tviews/src/main/java/vkraevskiy/com/tviews/TTextInputEditText.java
@@ -0,0 +1,21 @@
+package vkraevskiy.com.tviews;
+
+import android.content.Context;
+import android.support.design.widget.TextInputEditText;
+import android.util.AttributeSet;
+
+@SuppressWarnings("unused")
+public final class TTextInputEditText extends TextInputEditText {
+
+    public TTextInputEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypefaceUtil.initializeView(this, attrs, 0, 0);
+    }
+
+    public TTextInputEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypefaceUtil.initializeView(this, attrs, defStyleAttr, 0);
+    }
+}
